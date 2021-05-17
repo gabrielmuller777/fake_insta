65625b22e13d929c1a39f8a953e9959aa099999b
gabrielmuller777 <gabriel.ceolato@yahoo.com.br>
2021-05-17 20:24:31 -0300

    update

---
+++ Backend/main.py
@@ -0,0 +1,19 @@
+def follow_user():
+    pass
+
+
+def unfollow_user():
+    response = (input("Are you sure you want to unfollow this person? "))
+
+
+def like_photo():
+    pass
+
+
+def like_comment():
+    pass
+
+
+def message():
+    pass
+
