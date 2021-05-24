26436d32453de2bcad5b4eaf6b24d300ef1f2c71
FernandoItami <fernandoitami@gmail.com>
2021-05-24 21:00:00 +0900

    users folder , forms.py created ,views entry added

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
+from django.contrib.auth import login
+from django.urls import reverse
 from django.http import HttpResponse
+from .users.forms import RegisterForm
 # Create your views here.
 
 def index(request):
@@ -7,3 +10,16 @@ def index(request):
 
 def esbelus(request):
     return HttpResponse("Outra mensagem")
+
+def register(request):
+    if request.method == 'GET':
+        return render(
+            request, 'users/register.html',
+            {'form': RegisterForm}
+        )
+    elif request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('home'))
