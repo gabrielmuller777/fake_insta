b042a7fa63b04f1ceee9af88eb169c8bc2916b3b
FernandoItami <fernandoitami@gmail.com>
2021-06-06 12:05:05 +0900

    bootstrap

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include
 from .views import dashboard, register
 
 urlpatterns = [
-    path('dashboard/', dashboard, name='dashboard'),
+    path('', dashboard, name='dashboard'),
     path('accounts/',include('django.contrib.auth.urls')),
     path('register/', register, name='register'),
 ]
