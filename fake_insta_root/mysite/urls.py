4b30a3291771609dcea12149b8025212f13afea2
FernandoItami <fernandoitami@gmail.com>
2021-06-06 12:50:32 +0900

    merged

--- fake_insta_root/mysite/urls.py
+++ fake_insta_root/mysite/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls import include
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls'))
+    path('', include('home.urls'))
    #todo path('/', include())
 ]
